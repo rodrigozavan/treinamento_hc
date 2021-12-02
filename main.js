fc364b6a7acacb6f81e5cfe0c3429570dd2a37d7
rodrigozavan <https://github.com/rodrigozavan>
2021-12-02 20:16:22 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,105 @@
+const nameInput = document.querySelector("#name")
+const docInput = document.querySelector("#document")
+const genderInput = document.querySelector("#gender")
+const btnSend = document.querySelector("#send")
+const alertMsg = document.querySelector("#alert")
+let arrayDocs = document.querySelectorAll(".document")
+const tableUser = document.querySelector("#table-user")
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    addDeleteEvent() 
+})
+
+btnSend.addEventListener('click', ()=>{
+    if (!validInputsEmpty() || !validDuplicateDoc() || !validLenthDoc(docInput)) {
+        return false
+    }else{
+        addUser(nameInput, docInput, genderInput)
+        addDeleteEvent();
+    }
+})
+
+function addDeleteEvent() {
+    const btnDelete = document.querySelectorAll(".btn-delete")
+    btnDelete.forEach(element => {
+        element.addEventListener('click', deleteEvent(element))
+    });
+}
+
+function deleteEvent(element) {
+    element.addEventListener('click', (e)=>{
+        e.target.parentNode.parentNode.remove()
+    })
+}
+
+function validInputsEmpty() {
+    if (nameInput.value == '' || docInput.value == '') {
+        showMsg("error", "Preencha todos os campos.");
+        return false
+    }else{
+        return true
+    }
+}
+
+function showMsg(type, message) {
+    if (type == 'error') {
+        alertMsg.classList.add("alert-danger")
+        alertMsg.textContent = message
+        alertMsg.style.display = ''
+
+    } else if (type == 'success'){
+        alertMsg.classList.remove("alert-danger")
+        alertMsg.classList.add("alert-success")
+        alertMsg.textContent = message
+        alertMsg.style.display = ''
+    }
+}
+
+function validDuplicateDoc(){
+    arrayDocs = document.querySelectorAll(".document")
+    let docs = []
+    let currentDoc =  docInput.value.replaceAll('.', '').replaceAll('-', '')
+    arrayDocs.forEach(element => {
+        docs.push(element.textContent.replaceAll('.', '').replaceAll('-', ''))
+    });
+
+    if(docs.includes(currentDoc)){
+        showMsg('error', 'CPF já cadastrado.')
+        return false
+    }else{
+        return true
+    }
+    
+}
+
+function validLenthDoc(doc) {
+    let currentDoc =  doc.value.replaceAll('.', '').replaceAll('-', '')
+    if(currentDoc.length < 11 || currentDoc.length > 11){
+        showMsg('error', 'CPF Inválido!')
+        return false
+    }else{
+        return true
+    }
+}
+
+function addUser(name, doc, sex) {
+    if (sex.value == '1') {
+        sex = 'Masculino'
+    }else{
+        sex = 'Feminino'
+    }
+
+    let template = `
+    <th scope="row">3</th>
+    <td>${name.value}</td>
+    <td class="document">${doc.value}</td>
+    <td>${sex}</td>
+    <td>
+        <button type="button" class="btn btn-danger btn-delete">Excluir</button>
+    </td>
+    `
+    tr = document.createElement('tr')
+    tr.innerHTML = template
+    tableUser.appendChild(tr)
+    showMsg('success', 'Cadastrado com sucesso.')
+}
